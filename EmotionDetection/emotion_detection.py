bba465f1ea5c1ab8dfe7025787c7cd8219fd6961
puds <79672534+pudimzera@users.noreply.github.com>
2026-09-22 00:12:11 -0300

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,19 +3,27 @@ import requests
 
 
 def emotion_detector(text_to_analyse):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    url = (
+        "https://sn-watson-emotion.labs.skills.network/"
+        "v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    )
 
     headers = {
-        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+        "grpc-metadata-mm-model-id":
+        "emotion_aggregated-workflow_lang_en_stock"
     }
 
     input_json = {
         "raw_document": {
-            "text": text_to_analyze
+            "text": text_to_analyse
         }
     }
 
-    response = requests.post(url, headers=headers, json=input_json)
+    response = requests.post(
+        url,
+        headers=headers,
+        json=input_json
+    )
 
     if response.status_code == 400:
         return {
