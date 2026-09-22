1c052c0c614c8753210590604d30d460766760dc
puds <79672534+pudimzera@users.noreply.github.com>
2026-09-22 00:10:22 -0300

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 
-def emotion_detector(text_to_analyze):
+def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 
     headers = {
